cb4c558552ae37f58f8455507fa9b06d2269659b
wajahat414 <wajahatakram414@gmail.com>
2024-06-08 11:17:13 +0400

    Initial Commit

---
+++ coding_task/coding_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  coding_taskApp.swift
+//  coding_task
+//
+//  Created by devloper on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct coding_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
